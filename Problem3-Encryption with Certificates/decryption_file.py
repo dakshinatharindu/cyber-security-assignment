7e5072f036a34e9ddba8b7085e66f7caba6cc174
dakshinatharindu <tharinduokd@gmail.com>
2024-04-13 18:09:17 +0530

    complete decryption part

--- Problem3-Encryption with Certificates/decryption_file.py
+++ Problem3-Encryption with Certificates/decryption_file.py
@@ -2,25 +2,27 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import os
 
+
 def decrypt_file(encrypted_file_path, private_key_path, output_file_path):
     # Use the proper read write formats where necessary (its missing in the code)
     # Read the encrypted data
-    with open(encrypted_file_path,) as encrypted_file:
+    with open(encrypted_file_path, "rb") as encrypted_file:
         encrypted_data = encrypted_file.read()
 
     # Read the private key
-    with open(private_key_path,) as key_file:
+    with open(private_key_path, "r") as key_file:
         private_key = RSA.import_key(key_file.read())
 
     # Decrypt the data with RSA
-    cipher_rsa = .new()
-    decrypted_data = cipher_rsa.decrypt()
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    decrypted_data = cipher_rsa.decrypt(encrypted_data)
 
     # Write the decrypted data to the output file
-    with open(output_file_path,) as decrypted_file:
-        decrypted_file.write()
+    with open(output_file_path, "w") as decrypted_file:
+        decrypted_file.write(decrypted_data.decode("utf-8"))
 
     print(f'Decryption complete. Decrypted file saved to: {output_file_path}')
+
 
 if __name__ == "__main__":
     # Get user input
